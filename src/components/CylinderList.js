34b19eb454554157e9ecbc389af49e005f003f07
Shahriar Sakib <shahriarsakib.khan@gmail.com>
2025-01-25 15:29:58 +0600

    add allCylinders array and an unordered list to show names

--- src/components/CylinderList.js
+++ src/components/CylinderList.js
@@ -1,7 +1,17 @@
+const allCylinders = [
+  { id: 1, name: "Laughs Gas", size: 12, avaiable: true },
+  { id: 2, name: "Bashundhara LP Gas", size: 12, avaiable: false },
+  { id: 3, name: "Green LP Gas", size: 24, avaiable: true },
+  { id: 4, name: "Omera LPG", size: 24, avaiable: false },
+  { id: 5, name: "Delta LPG", size: 12, avaiable: true },
+  { id: 6, name: "Fresh LP Gas", size: 12, avaiable: true },
+  { id: 7, name: "Jamuna Gas", size: 12, avaiable: true },
+];
+
 export default function CylinderList() {
   return (
     <div>
-      <h1> I'm lists </h1>
+      <ul>{allCylinders.map((cylinder) => cylinder.name + " ")}</ul>
     </div>
   );
 }
