70873adced69fc0f7743837b68c776894d8cf779
Shahriar Sakib <shahriarsakib.khan@gmail.com>
2025-01-26 15:28:21 +0600

    edit images and add them in the CylinderList

--- src/components/CylinderList.js
+++ src/components/CylinderList.js
@@ -1,13 +1,55 @@
 import Cylinder from "./Cylinder";
 
 const allCylinders = [
-  { id: 1, name: "Laughs Gas", size: 12, avaiable: true },
-  { id: 2, name: "Bashundhara LP Gas", size: 12, avaiable: false },
-  { id: 3, name: "Green LP Gas", size: 24, avaiable: true },
-  { id: 4, name: "Omera LPG", size: 24, avaiable: false },
-  { id: 5, name: "Delta LPG", size: 12, avaiable: true },
-  { id: 6, name: "Fresh LP Gas", size: 12, avaiable: true },
-  { id: 7, name: "Jamuna Gas", size: 12, avaiable: true },
+  {
+    id: 1,
+    name: "Laughs Gas",
+    size: 12,
+    isAvaiable: true,
+    picture: "./public/images/laughs",
+  },
+  {
+    id: 2,
+    name: "Bashundhara LP Gas",
+    size: 12,
+    isAvaiable: false,
+    picture: "./public/images/bashundhara",
+  },
+  {
+    id: 3,
+    name: "Green LP Gas",
+    size: 24,
+    isAvaiable: true,
+    picture: "./public/images/omera",
+  },
+  {
+    id: 4,
+    name: "Omera LPG",
+    size: 24,
+    isAvaiable: false,
+    picture: "./public/images/omera",
+  },
+  {
+    id: 5,
+    name: "Delta LPG",
+    size: 12,
+    isAvaiable: true,
+    picture: "./public/images/delta",
+  },
+  {
+    id: 6,
+    name: "Fresh LP Gas",
+    size: 12,
+    isAvaiable: true,
+    picture: "./public/images/fresh",
+  },
+  {
+    id: 7,
+    name: "Jamuna Gas",
+    size: 12,
+    isAvaiable: true,
+    picture: "./public/images/jamuna",
+  },
 ];
 
 export default function CylinderList() {
