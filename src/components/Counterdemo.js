305c05995c8afad4a8417e21a50ab7bc8b42217c
Shahriar Sakib <shahriarsakib.khan@gmail.com>
2025-01-27 13:39:17 +0600

    create and add step

--- src/components/Counterdemo.js
+++ src/components/Counterdemo.js
@@ -1,25 +1,45 @@
 import { useState } from "react";
 export default function Counterdemo() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
   function handleCount(change) {
     setCount((count) => count + change);
   }
+  function handleStep(change) {
+    setStep((step) => step + change);
+  }
 
   return (
-    <div
-      className="counter"
-      tabIndex="0"
-      onKeyDown={(e) => {
-        if (e.key === "ArrowDown" && count > 0) handleCount(-1);
-        if (e.key === "ArrowUp") handleCount(1);
-      }}
-    >
-      <button onClick={() => handleCount(-1)} disabled={count <= 0}>
-        -
-      </button>
-      <span>{count}</span>
-      <button onClick={() => handleCount(1)}>+</button>
+    <div>
+      <div
+        className="counter"
+        tabIndex="0"
+        onKeyDown={(e) => {
+          if (e.key === "ArrowDown" && step > 0) handleStep(-1);
+          if (e.key === "ArrowUp") handleStep(1);
+        }}
+      >
+        <button onClick={() => handleStep(-1)} disabled={step <= 0}>
+          -
+        </button>
+        <span>Step: {step}</span>
+        <button onClick={() => handleStep(1)}>+</button>
+      </div>
+      <div
+        className="counter"
+        tabIndex="0"
+        onKeyDown={(e) => {
+          if (e.key === "ArrowDown" && count > 0) handleCount(-step);
+          if (e.key === "ArrowUp") handleCount(step);
+        }}
+      >
+        <button onClick={() => handleCount(-step)} disabled={count - step < 0}>
+          -
+        </button>
+        <span>Count: {count}</span>
+        <button onClick={() => handleCount(step)}>+</button>
+      </div>
     </div>
   );
 }
